d1fa592901ef3b90543095fb4fc79b22f0f3a80f
JesAquino <aquinojesuael12@gmail.com>
2023-02-16 21:57:20 +0400

    POST and GET

--- server.js
+++ server.js
@@ -1,15 +1,76 @@
 const express = require('express');
+
+const dotenv = require ('dotenv');
+
+const cors = require ('cors');
+
+const path = require('path');
+
+const MongoClient = require ('mongodb').MongoClient;
+
+dotenv.config({
+    path: './config.env',
+})
+
+
 //Create an Express.js instance
 const app = express();
 
-app.get('/', (req,res)=>{
-    res.send("Hello");
-});
+const PORT = process.env.PORT || 5000;
 
 
-app.get('/employees',(req,res)=>{
-    res.send('Employees')
-})
 
-app.listen(3000);
+//Connecting to the MongoDB
+const uri = process.env.MONGO_URI;
+
+MongoClient.connect(url)
+.then(client=>{
+    console.log('Connected to MongoDB');
+
+    //Selecting the Database and Collections
+    const db = client.db('CW_2');
+    const lesson_information = db.collection('lesson_information');
+
+    
+    app.use(cors())
+    app.use(express.json())
+
+    app.listen(PORT,()=>console.log('Server listening in port:'+PORT));
+    })
+
+
+    //POST or Creating records for for lesson
+    app.post('lesson_information',(req,res)=>{
+       CW_2.insertOne(req.body)
+       .then(result=>{
+        res.status(201).json({
+            success:true,
+        })
+       })
+        .catch(err=>{
+            res.status(500).json({
+                success:false,
+            })
+        })
+    })
+
+    //GET for getting all lesson information
+    app.get('/lesson_information', (req, res) => {
+        lesson_information
+        .find({})
+        .toArray()
+        .then((results)=>{
+            res.status(200).json({
+                success:true,
+                data:results,
+            });
+        });
+    })
+
+    .catch(error=>{
+        console.log(error.mesasge);
+    })
+    
+    
+
 
